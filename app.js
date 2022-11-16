ec5301d30e751607c6a6548f74c56e0c3187f524
mohammadyeasain123 <110459502+mohammadyeasain123@users.noreply.github.com>
2022-11-16 12:55:43 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+$(function(){
+
+  $('.natureimg').slick({
+    prevArrow:"#nature .arrows .larrows",
+    nextArrow:"#nature .arrows .rarrows",
+  });
+
+
+
+  $('.naturebeautyslider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow:"#naturebeauty .arrows .larrows",
+    nextArrow:"#naturebeauty .arrows .rarrows",
+  });
+
+
+  
+
+
+})
